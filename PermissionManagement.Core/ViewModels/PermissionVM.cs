de471fef8e8623f32adf64e2a63a31b8739188f6
Mostafa Abdelrahman <43328278+Mustafa-Abdelrahman@users.noreply.github.com>
2022-04-07 00:52:32 +0200

    Add View Models

---
+++ PermissionManagement.Core/ViewModels/PermissionVM.cs
@@ -0,0 +1,8 @@
+﻿namespace PermissionManagement.Web.ViewModels
+{
+    public class PermissionVM
+    {
+        public string ViewName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ PermissionManagement.Core/ViewModels/PermissionsViewVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PermissionManagement.Web.ViewModels
+{
+    public class PermissionsViewVM
+    {
+        public List<IdentityUser> Users { get; set; }
+        public List<PermissionVM> Permissions { get; set; }
+    }
+}
